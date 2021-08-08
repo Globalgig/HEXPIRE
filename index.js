1d5f079713335dd716959333ddfbb8d8366fd684
Alex Southard <00alexsouthard@gmail.com>
2021-08-08 17:57:59 -0600

    sound timing test

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+function startup(){
+	document.getElementById("loadingSound").play();
+	addFade("red", "redFade")
+	addFade("orange", "orangeFade")
+	addFade("blue", "blueFade")
+	addFade("purple", "purpleFade")
+	addFade("green", "greenFade")
+	addFade("yellow", "yellowFade")
+	window.removeEventListener("click", startup);
+}
+
+function addFade(id, fadeclass){
+	obj = document.getElementById(id);
+	obj.style.webkitAnimationName = fadeclass;
+	obj.style.webkitAnimationDuration = "2.3s";
+	obj.style.opacity = 1;	
+}
+
+window.addEventListener("click", startup);
+
 function increment(){
 	value = document.getElementById("theButton").value;
 	value++;
 	document.getElementById("theButton").innerHTML = value;
 	document.getElementById("theButton").value = value;
 }
-
-window.onload = function() {
-	alert("Okay!");
-	document.getElementById("loadingSound").play();
-}
-
-
-window.addEventListener("click", function(event) {
-    document.getElementById("loadingSound").play();
-});
 
 
 
